f569801d1455326d86f4023fcf427eda337efd0e
andrew264 <andrewsanthosh264@gmail.com>
2023-02-23 19:29:11 +0530

    a webpage ☕🐿️

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from utils.fetch import fetch_news
 
 app = Flask(__name__)
 
 
-@app.route('/')
-async def hello_world():
-    return render_template('index.html', name='Flask',)
+@app.route('/', methods=['GET', 'POST'])
+async def index():
+    if request.method == 'POST':
+        query = request.form['query']
+        category = request.form['category']
+        category = None if category == 'all' else category
+        print(query, category)
+        results = await fetch_news(query, category)
+        print(len(results))
+        return render_template('index.html', articles=results)
+    else:
+        return render_template('index.html', results=[])
