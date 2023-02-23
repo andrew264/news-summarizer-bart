bade36a7062bc2734de6a61d25577d6123629025
andrew264 <andrewsanthosh264@gmail.com>
2023-02-23 15:22:32 +0530

    fetch news asynchronously

--- utils/fetch.py
+++ utils/fetch.py
@@ -1,11 +1,12 @@
+import asyncio
 import json
-from multiprocessing import Queue
+from typing import Optional
 
 from newsapi import NewsApiClient
 
 from utils.article import Article
 
-with open('../config.json') as f:
+with open('config.json') as f:
     config = json.load(f)
     API_KEY = config['newsapi-key']
 
@@ -14,9 +15,14 @@ newsapi = NewsApiClient(api_key=API_KEY)
 SOURCES: list[str] = ['bbc-utils', 'the-verge', 'techcrunch']
 
 
-def fetch_news(search_term: str, category: str, queue: Queue):
+def _fetch_news(search_term: str, category: Optional[str], ):
     sources = None if search_term else ', '.join(SOURCES)
     print(f'Fetching news for {search_term} in {category} from {sources}...')
     results = newsapi.get_top_headlines(q=search_term, sources=sources, category=category, page_size=10, page=1)
+    return [Article(article) for article in results['articles']]
 
-    queue.put([Article(article) for article in results['articles']])
+
+async def fetch_news(search_term: str, category: Optional[str] = None):
+    event_loop = asyncio.get_event_loop()
+    results = await event_loop.run_in_executor(None, _fetch_news, search_term, category, )
+    return results
